5611190ba80b3b0dd437e881a7e73a4efd93bd6b
Jenn Lao <jenn.p.lao@gmail.com>
2018-12-12 09:22:46 -0700

    converts user story to test

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -9,3 +9,25 @@ feature "User can visit root page" do
     end
   end
 end
+
+describe 'as a user' do
+  it 'can visit the root page' do
+
+    visit "/"
+    fill_in 'q', with: '80203'
+    click_on "Locate"
+
+    expect(current_path).to eq(search_path)
+    expect(station.count).to eq 10
+    #Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+    expect(['Electric', 'Propane']).to include(station[0].fuel_type)
+    expect(station[5].fuel_type).not_to eq('Gas')
+    within('.station1')do
+      expect(page).to have_content(station_1.name)
+      expect(page).to have_content(station_1.address)
+      expect(page).to have_content(station_1.fuel_type)
+      expect(page).to have_content(station_1.distance)
+      expect(page).to have_content(station_1.access_times)
+    end
+  end
+end
